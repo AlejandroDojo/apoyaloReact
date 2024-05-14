620dc216658affe1ec6d73730a403f3aecfc12bc
AlejandroDojo <ultiimately@gmail.com>
2024-05-14 12:58:31 -0400

    Of class to funcional

--- src/components/PersonCard/PersonCard.jsx
+++ src/components/PersonCard/PersonCard.jsx
@@ -1,16 +1,10 @@
-import { Component } from "react"
+import React from "react";
 
-
-class PersonCard extends Component {
-    render = () => {
-        const {name, lastName, age, hairColor} = this.props;
-        return (
-            <div>
-            <h2>{name}, {lastName}</h2>
-            <p>Age: {age}</p>
-            <p>Hair Color: {hairColor}</p>
-            </div>
-        )
-    }
+const PersonCard = ({name, lastName, age, hairColor}) => {
+    return (<div>
+        <h2>{name}, {lastName}</h2>
+        <p>Age: {age}</p>
+        <p>Hair Color: {hairColor}</p>
+        </div>);
 }
 export default PersonCard;
